c4e4ec552ea4443a850290ff76ee600d33ef3374
MaruchanPark <48475993+MaruchanPark@users.noreply.github.com>
2021-05-31 00:21:21 +0900

    Update 1992 - 쿼드트리.py

--- Baekjoon Online Judge/1992 - 쿼드트리.py
+++ Baekjoon Online Judge/1992 - 쿼드트리.py
@@ -1,41 +1,64 @@
-n = int(input())
+'''
+주어진 영상이 모두 0으로만 되어 있으면 압축 결과는 "0"이 되고, 모두 1로만 되어 있으면 압축 결과는 "1"이 된다. 
 
-arr = []
-for i in range(n):
-    arr.append(input())
+만약 0과 1이 섞여 있으면 전체를 한 번에 나타내지를 못하고, 왼쪽 위, 오른쪽 위, 왼쪽 아래, 오른쪽 아래,
+이렇게 4개의 영상으로 나누어 압축하게 되며, 이 4개의 영역을 압축한 결과를 차례대로 괄호 안에 묶어서 표현한다.
 
+입력:
+첫째 줄에는 영상의 크기를 나타내는 숫자 N 이 주어진다. N 은 언제나 2의 제곱수로 주어지며,
+1 ≤ N ≤ 64의 범위를 가진다. 두 번째 줄부터는 길이 N의 문자열이 N개 들어온다.
+각 문자열은 0 또는 1의 숫자로 이루어져 있으며, 영상의 각 점들을 나타낸다.
 
+출력:
+영상을 압축한 결과를 출력한다.
 
-def mark_split(array, N):
+문제링크:
+https://www.acmicpc.net/problem/1992
+'''
 
-    cnt_0 = 0
-    cnt_1 = 0
-    for array_ in array:
-        if '0' in array_:
-            cnt_0 = 1
-        if '1' in array_:
-            cnt_1 = 1
-    if cnt_0 == 0:
-        print('1', end = '')
-    elif cnt_1 == 0:
-        print('0', end = '')
+import sys
 
+N = int(input())
+map_ = []
+for i in range(N):
+    map_.append(sys.stdin.readline().rstrip())
+
+def check(x, y, n):
+    #입력의 첫 번째 값과 모든 값을 비교
+    base = map_[y][x]
+    ext_idx = 1
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if base != map_[j][i]:
+                ext_idx = 0
+                break
+
+        if ext_idx == 0:
+            break
+    #모두 같으면 압축 (출력)
+    if ext_idx == 1:
+        print(base, end = '')
+        return
+    #다르면 분할하여 비교
     else:
-        print('(',end = '')
-        array_1 = []
-        array_2 = []
-        array_3 = []
-        array_4 = []
-        for i in range(N//2):
-            array_1.append(array[i][0:N//2])
-            array_2.append(array[i][N//2:N])
-            array_3.append(array[i+N//2][0:N//2])
-            array_4.append(array[i+N//2][N//2:N])
+        print('(', end = '')
+        #입력 4분할 위해 범위 설정하여 함수 실행
+        x_ = x
+        y_ = y
+        check(x_, y_, n//2)
 
-        mark_split(array_1, N//2)
-        mark_split(array_2, N//2)
-        mark_split(array_3, N//2)
-        mark_split(array_4, N//2)
+        x_ = x + n // 2
+        y_ = y
+        check(x_, y_, n//2)
+
+        x_ = x
+        y_ = y + n // 2
+        check(x_, y_, n//2)
+
+        x_ = x + n // 2
+        y_ = y + n // 2
+        check(x_, y_, n//2)
         print(')', end = '')
 
-mark_split(arr, n)
+
+check(0,0, N)
