ac44eda2f7c9b29fc31302d6de28b392be4846c4
MaruchanPark <shaild098@naver.com>
2024-04-21 07:12:05 +0000

    add question picker

---
+++ Baekjoon Online Judge/question_picker.py
@@ -0,0 +1,16 @@
+import random
+
+questions = [
+    "https://www.acmicpc.net/problem/12865",
+    "https://www.acmicpc.net/problem/9251",
+    "https://www.acmicpc.net/problem/7569",
+    "https://www.acmicpc.net/problem/9663",
+    "https://www.acmicpc.net/problem/2580",
+    "https://www.acmicpc.net/problem/2447",
+    "https://www.acmicpc.net/problem/11729",
+    "https://www.acmicpc.net/problem/2110",
+    "https://www.acmicpc.net/problem/1806",
+    "https://www.acmicpc.net/problem/1655",
+]
+
+print(random.choice(questions))
